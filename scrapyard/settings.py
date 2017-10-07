738b98297f3c9acc0f5f7a9e606ea3b3267f316b
antonyCastieniras <antonycastineiras@gmail.com>
2017-10-07 12:26:02 +0100

    updated

--- scrapyard/settings.py
+++ scrapyard/settings.py
@@ -122,9 +122,6 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.11/howto/static-files/
-
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
